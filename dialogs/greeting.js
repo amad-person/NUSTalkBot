004470bf49a9ded860d80f37d95295a2b2bfc92f
kush1509 <kushagra.goel.15@gmail.com>
2017-07-23 21:33:54 +0530

    ivle

--- dialogs/greeting.js
+++ dialogs/greeting.js
@@ -4,11 +4,30 @@ module.exports = function () {
    		if (IVLEToken) {
             session.token = IVLEToken.entity;
 			session.send(session.token);
-			http.get('https://ivle.nus.edu.sg/api/Lapi.svc/UserName_Get?APIKey=JWE5l4plZpPkhqENrgaVx&Token='+ session.token, { cache: true })
-				.then(function(result){
+			/*
+			http.get('http://ivle.nus.edu.sg/api/Lapi.svc/UserName_Get?APIKey=JWE5l4plZpPkhqENrgaVx&Token='+ session.token, { cache: true })
+				.(function(result){
 					console.log(result);
 					session.userData.about.name = result.data;
 					session.send(session.userData.about.name);
+			});
+			*/
+			var options = {
+			  host: 'ivle.nus.edu.sg',
+			  port: 80,
+			  path: '/api/Lapi.svc/UserName_Get?APIKey=JWE5l4plZpPkhqENrgaVx&Token='+ session.token
+			};
+
+			http.get(options, function(res) {
+			  console.log("Got response: " + res.statusCode);
+			  session.userData.about.name = res.data;
+			  session.send(session.userData.about.name);
+
+			  res.on("data", function(chunk) {
+			    console.log("BODY: " + chunk);
+			  });
+			}).on('error', function(e) {
+			  console.log("Got error: " + e.message);
 			});
         }
         session.send("Hey %s! I'm the NUSTalkBot. Type something.", session.userData.about.name);

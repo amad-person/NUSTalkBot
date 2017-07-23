da8935a53078d2c1299ffb73df0f44673bdc8921
kush1509 <kushagra.goel.15@gmail.com>
2017-07-23 20:06:52 +0530

    ivle

--- dialogs/greeting.js
+++ dialogs/greeting.js
@@ -1,5 +1,10 @@
 module.exports = function () {
-    bot.dialog('greetingDialog', function (session) {
+    bot.dialog('greetingDialog', function (session, args) {
+    	var IVLEToken= builder.EntityRecognizer.findEntity(args.intent.entities, 'IVLEtoken');
+   		if (IVLEtoken) {
+            session.userData.token = IVLEToken.entity;
+			session.send(session.userData.token);
+        }
         session.send("Hey %s! I'm the NUSTalkBot. Type something.", session.userData.about.name);
         session.send("If you're using this for the first time or want to see the welcome message again, type \'start\'.");
     }).triggerAction({

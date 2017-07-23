84df31f945e1ffce4a0d4202bf560f8aa4412970
kush1509 <kushagra.goel.15@gmail.com>
2017-07-23 19:33:24 +0530

    ivle

--- dialogs/welcome.js
+++ dialogs/welcome.js
@@ -1,9 +1,13 @@
 module.exports = function () {
     bot.dialog('welcomeDialog', function (session, args) {
         session.sendTyping();
-
-   		var n = builder.EntityRecognizer.findEntity(args.intent.entities, 'IVLEtoken')
-   		session.send(n);
+		
+		var IVLEToken= builder.EntityRecognizer.findEntity(args.intent.entities, 'IVLEtoken');
+   		if (n) {
+            session.userData.token = IVLEToken.entity;
+            
+            //session.send(session.userData.token);
+        }
         session.send("%s, I have some information for you.", session.userData.about.name);
         session.send("Modules you've taken for this semester: %s", session.userData.about.moduleNames.toString());
         session.send("Commands you can type:  \nask: ask questions after selecting a module  \ntimetable: ask questions related to your lecture schedule  \nhistory: see a list of links you've visited today  \nrestart: restart your conversation");

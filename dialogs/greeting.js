4d325d0a4ffe8ce11136f50b6a2e3fbb1379d91a
kush1509 <kushagra.goel.15@gmail.com>
2017-07-24 02:36:39 +0530

    ivle

--- dialogs/greeting.js
+++ dialogs/greeting.js
@@ -24,8 +24,13 @@ module.exports = function () {
          	  	session.send('error');
 		       	console.log(error);
 		       } else{
-		            session.send('mo');
-		            //session.userData.about.moduleNames = [];
+		            console.log(JSON.parse(response.body).Results);
+		            session.userData.about.modules = JSON.parse(JSON.parse(response.body).Results);
+		            session.send("modules", session.userData.about.modules);
+		            
+		            for(var i=0;i<session.userData.about.modules.length;++i){
+		            	session.userData.about.moduleNames.push(session.userData.about.modules[i].CourseCode);
+		            }
 		        }
 			});
 

6b7672598fcc1ccdc965315b5b5d980244a9ee50
kush1509 <kushagra.goel.15@gmail.com>
2017-07-23 17:35:22 +0530

    ivle

--- dialogs/welcome.js
+++ dialogs/welcome.js
@@ -2,7 +2,7 @@ module.exports = function () {
     bot.dialog('welcomeDialog', function (session, args) {
         session.sendTyping();
 
-        session.send("%s", args);
+        console.log(args);
         session.send("%s, I have some information for you.", session.userData.about.name);
         session.send("Modules you've taken for this semester: %s", session.userData.about.moduleNames.toString());
         session.send("Commands you can type:  \nask: ask questions after selecting a module  \ntimetable: ask questions related to your lecture schedule  \nhistory: see a list of links you've visited today  \nrestart: restart your conversation");
